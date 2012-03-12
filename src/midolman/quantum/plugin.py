fe5b4576186be50755c84621d9046bbae6d51030
Tomoe Sugihara <tomoe@midokura.com>
2012-03-12 15:26:21 +0900

    Fix small things
    Make get_all_networks() work with dummy network data
    Change print to LOG.debug so it shows up on the screen

--- src/midolman/quantum/plugin.py
+++ src/midolman/quantum/plugin.py
@@ -17,6 +17,8 @@
 
 import logging
 from quantum.quantum_plugin_base import QuantumPluginBase
+from quantum.api.api_common import OperationalStatus
+
 
 LOG = logging.getLogger('midolman.quantum.plugins.MidonetPlugin')
 
@@ -27,50 +29,55 @@ class MidonetPlugin(QuantumPluginBase):
     This is a skeleton that is copied from QuantumEchoPlugin in SamplePlugin.
     """
 
-    def get_all_networks(self, tenant_id):
+    def get_all_networks(self, tenant_id, filter_opts=None):
         """
         Returns a dictionary containing all
         <network_uuid, network_name> for
         the specified tenant.
         """
-        print("get_all_networks() called\n")
+        LOG.debug("get_all_networks() called with tenant_id %r", tenant_id)
+
+        dummy_network = {'net-id': "fake-network",
+                'net-name': "fake-name",
+                'net-op-status': OperationalStatus}
+        return [dummy_network]
 
     def create_network(self, tenant_id, net_name, **kwargs):
         """
         Creates a new Virtual Network, and assigns it
         a symbolic name.
         """
-        print("create_network() called\n")
+        LOG.debug("create_network() called\n")
 
     def delete_network(self, tenant_id, net_id):
         """
         Deletes the network with the specified network identifier
         belonging to the specified tenant.
         """
-        print("delete_network() called\n")
+        LOG.debug("delete_network() called\n")
 
     def get_network_details(self, tenant_id, net_id):
         """
         Deletes the Virtual Network belonging to a the
         spec
         """
-        print("get_network_details() called\n")
+        LOG.debug("get_network_details() called\n")
 
     def update_network(self, tenant_id, net_id, **kwargs):
-        print("update_network() called")
+        LOG.debug("update_network() called")
 
     def get_all_ports(self, tenant_id, net_id):
         """
         Retrieves all port identifiers belonging to the
         specified Virtual Network.
         """
-        print("get_all_ports() called\n")
+        LOG.debug("get_all_ports() called\n")
 
     def create_port(self, tenant_id, net_id, **kwargs):
         """
         Creates a port on the specified Virtual Network.
         """
-        print("create_port() called\n")
+        LOG.debug("create_port() called\n")
 
     def delete_port(self, tenant_id, net_id, port_id):
         """
@@ -79,38 +86,38 @@ class MidonetPlugin(QuantumPluginBase):
         the remote interface is first un-plugged and then the port
         is deleted.
         """
-        print("delete_port() called\n")
+        LOG.debug("delete_port() called\n")
 
     def update_port(self, tenant_id, net_id, port_id, **kwargs):
         """
         Updates the attributes of a port on the specified Virtual Network.
         """
-        print("update_port() called\n")
+        LOG.debug("update_port() called\n")
 
     def get_port_details(self, tenant_id, net_id, port_id):
         """
         This method allows the user to retrieve a remote interface
         that is attached to this particular port.
         """
-        print("get_port_details() called\n")
+        LOG.debug("get_port_details() called\n")
 
     def plug_interface(self, tenant_id, net_id, port_id, remote_interface_id):
         """
         Attaches a remote interface to the specified port on the
         specified Virtual Network.
         """
-        print("plug_interface() called\n")
+        LOG.debug("plug_interface() called\n")
 
     def unplug_interface(self, tenant_id, net_id, port_id):
         """
         Detaches a remote interface from the specified port on the
         specified Virtual Network.
         """
-        print("unplug_interface() called\n")
+        LOG.debug("unplug_interface() called\n")
 
     supported_extension_aliases = ["FOXNSOX"]
 
     def method_to_support_foxnsox_extension(self):
-        print("method_to_support_foxnsox_extension() called\n")
+        LOG.debug("method_to_support_foxnsox_extension() called\n")
 
 
